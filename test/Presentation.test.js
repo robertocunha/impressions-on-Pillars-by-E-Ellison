998003fb968409cb79185ce82a2731349ca580c7
Roberto Cunha <robertocunha@gmail.com>
2024-08-14 19:09:38 -0300

    Use React act

--- test/Presentation.test.js
+++ test/Presentation.test.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Presentation } from "../src/Presentation";
+// https://react.dev/warnings/react-dom-test-utils
+import { act } from "react-dom/test-utils";
 
 describe("Presentation", () => {
     it("Shows the track name", () => {
         const container = document.createElement("div");
         document.body.appendChild(container);
 
-        const track = { trackName: "Maria"}
+        const track = { trackName: "Maria" }
         const component = <Presentation track={track} />;
 
-        ReactDOM.createRoot(container).render(component);
+        act(() =>
+            ReactDOM.createRoot(container).render(component)
+        );
 
         expect(document.body.textContent).toContain("Maria");
-    }) 
+    })
 });
